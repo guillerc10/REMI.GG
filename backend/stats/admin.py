e176850587684d09ecfccd73b49c4056df405bdc
guillerc10 <guillermocaceres164@gmail.com>
2026-09-04 18:14:41 -0400

    instalacion correcta del django

--- backend/stats/admin.py
+++ backend/stats/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Invocador, Liga, Partida, Campeon, Participante
 
-# Register your models here.
+admin.site.register(Invocador)
+admin.site.register(Liga)
+admin.site.register(Partida)
+admin.site.register(Campeon)
+admin.site.register(Participante)
