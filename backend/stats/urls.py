8c89114391f06d76d73d7bee50b572121e6c0f71
guillerc10 <guillermocaceres164@gmail.com>
2026-09-04 21:53:00 -0400

    PRUEBAS CON POSTMAN Y NEWMAN APROBADAS

---
+++ backend/stats/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("invocador/<str:game_name>/<str:tag_line>/", views.perfil_invocador),
+    path("invocador/<str:game_name>/<str:tag_line>/ligas/", views.ligas_invocador),
+    path("invocador/<str:game_name>/<str:tag_line>/winrate/", views.winrate_invocador),
+    path("invocador/<str:game_name>/<str:tag_line>/kda/", views.kda_invocador),
+    path("invocador/<str:game_name>/<str:tag_line>/campeones/", views.campeones_invocador),
+    path("invocador/<str:game_name>/<str:tag_line>/companeros/", views.companeros_invocador),
+]
